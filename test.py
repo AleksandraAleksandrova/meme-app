bdc34585dc0880e4f63496d976f9bd751f032370
AleksandraAleksandrova <a.aleksandrova2004@gmail.com>
2023-04-04 18:04:11 +0300

    quick fix

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print("This file will be used to push changes to the repo and check if the app responds")
 print("Had to update app permissions and agree to them for the meme-app repo")
+print("Change the push event check")
